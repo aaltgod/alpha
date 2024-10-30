5ab6259f5c5526514643f9ea768a04697a661cfe
aaltgod <minealaska29@gmail.com>
2024-10-31 00:33:41 +0300

    [ALPHA-7] feat: fix streams scrolling; fix cards padding

--- frontend/pages/streams/index.tsx
+++ frontend/pages/streams/index.tsx
@@ -72,29 +72,6 @@ export default defineComponent({
             flag_regexp: "FLAG==",
             color: "#color",
           },
-          {
-            direction: "OUT",
-            payload:
-              "HTTP/1.1 404 Not Found\ncontent-length: 0\ndate: Sun, 19 May 2024 13:47:01 GMT\n\n\n",
-            at: "13:00:15",
-            flag_regexp: "FLAG==",
-            color: "#color",
-          },
-          {
-            direction: "IN",
-            payload: `GET /get-streams-by-service-ids HTTP/1.1\nContent-Type: application/json\nUser-Agent: PostmanRuntime/7.39.0\nAccept: */*\nPostman-Token: 833d5c87-2f2f-4a88-9d74-ca4a9026a2f4\nHost: localhost:2137\nAccept-Encoding: gzip, deflate, br\nConnection: keep-alive Content-Length: 28\n{ "service_ids": [1] }`,
-            at: "13:00:16",
-            flag_regexp: "FLAG==",
-            color: "#color",
-          },
-          {
-            direction: "OUT",
-            payload:
-              "HTTP/1.1 404 Not Found\ncontent-length: 0\ndate: Sun, 19 May 2024 13:47:01 GMT\n\n\n",
-            at: "13:00:17",
-            flag_regexp: "FLAG==",
-            color: "#color",
-          },
         ],
       };
       rows.push(newRow);
@@ -108,16 +85,16 @@ export default defineComponent({
     };
 
     return () => (
-      <div class="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+      <div class="grid auto-rows-max items-start gap-4">
         <div
-          class="grid min-h-screen gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2"
-          style="grid-template-columns: 30% 70%"
+          class="grid gap-4 h-screen sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2"
+          style="grid-template-columns: 500px 1fr"
         >
           <Card>
-            <ScrollArea class="h-screen flex">
+            <ScrollArea class="h-screen">
               <CardContent>
                 <Table>
-                  <TableHeader>
+                  <TableHeader class="sticky top-0 bg-muted/100">
                     <TableRow>
                       <TableHead>№</TableHead>
                       <TableHead>Service</TableHead>
@@ -161,24 +138,26 @@ export default defineComponent({
               </CardContent>
             </ScrollArea>
           </Card>
-          <ScrollArea class="h-screen flex">
-            <Card>
-              <CardHeader class="flex flex-row items-start bg-muted/50">
-                <div class="grid gap-0.5">
-                  <CardTitle class="group flex items-center gap-2 text-lg">
-                    № {selectedRow.value.number}
-                  </CardTitle>
-                  <CardDescription>
-                    Количестов пакетов: {selectedRow.value.packets.length}
-                  </CardDescription>
-                </div>
-              </CardHeader>
+          <Card>
+            <CardHeader class="flex flex-row items-start bg-muted/50">
+              <div class="grid gap-0.5">
+                <CardTitle class="group flex items-center gap-2 text-lg">
+                  № {selectedRow.value.number}
+                </CardTitle>
+                <CardDescription>
+                  Количестов пакетов: {selectedRow.value.packets.length}
+                </CardDescription>
+              </div>
+            </CardHeader>
+            <ScrollArea>
               <CardContent class="p-6 text-sm">
                 {selectedRow.value.packets.map((packet) => (
                   <div>
                     <div class="border-b">
                       <div class="grid gap-3">
-                        <div class="font-semibold">{packet.at}</div>
+                        <div class="font-semibold">
+                          {packet.at} {packet.direction}
+                        </div>
                         <div class="flex-1 whitespace-pre-wrap p-4 text-sm">
                           {packet.payload}
                         </div>
@@ -188,8 +167,8 @@ export default defineComponent({
                   </div>
                 ))}
               </CardContent>
-            </Card>
-          </ScrollArea>
+            </ScrollArea>
+          </Card>
         </div>
       </div>
     );
